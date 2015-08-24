a99e37fd305c3c8d14c1f4941c76a4fdd910afa5
Tim.Tang <tang.jilong@139.com>
2015-08-24 10:42:23 +0800

    Patch mixlib for smartos.

--- lib/mixlib/install.rb
+++ lib/mixlib/install.rb
@@ -99,6 +99,11 @@ module Mixlib
 
     private
 
+    def is_smartos 
+      platform = ENV['CHEF_CURRENT_PLATFORM']
+      !platform.nil? && platform.downcase == "smartos"
+    end
+
     # Generates the install command variables for Bourne shell-based
     # platforms.
     #
@@ -162,10 +167,11 @@ module Mixlib
     end
 
     def shell_code_from_file(vars)
+      inst_file = is_smartos? "install_smartos" : "install_command"
       fn = File.join(
         File.dirname(__FILE__),
         %w[.. .. support],
-        "install_command"
+        inst_file
       )
       Util.shell_code_from_file(vars, fn, powershell,
                                 http_proxy: http_proxy, https_proxy: https_proxy)
---
+++ support/install_smartos.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+## Install PKG-SRC if not present
+if [ ! -d /opt/local ]; then
+  echo "nameserver 114.114.114.114" >> /etc/resolv.conf
+  cd /
+##curl -sk https://pkgsrc.joyent.com/packages/SmartOS/bootstrap/bootstrap-2015Q1-x86_64.tar.gz | gzcat | tar -xvf -
+  wget --no-check-certificate https://pkgsrc.joyent.com/packages/SmartOS/bootstrap/bootstrap-2015Q1-x86_64.tar.gz
+  gzcat bootstrap-2015Q1-x86_64.tar.gz | tar -xvf -
+  /opt/local/sbin/pkg_admin rebuild
+  /opt/local/bin/pkgin -y up
+fi
+
+# Install Ruby & Gems
+if [ ! -f /opt/local/bin/chef-client ]; then
+    /opt/local/bin/pkgin -y install gcc47 gcc47-runtime gmake ruby21-base ruby21-mime-types ruby21-yajl-ruby ruby21-nokogiri ruby21-readline coreutils pkg-config
+    echo "gemhome: /var/.gem" >> /root/.gemrc
+    gem sources -r https://rubygems.org/
+    gem sources -a http://ruby.taobao.org
+    gem install omnibus
+fi
+
+# augment path in an attempt to find a download program
+PATH="${PATH}:/smartdc/bin:/opt/smartdc/bin:/opt/local/bin:/opt/local/sbin:/opt/smartdc/agents/bin";
+export PATH;
