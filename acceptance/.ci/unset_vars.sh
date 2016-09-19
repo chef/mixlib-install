c48f117577336e73d00e1909c486d8c7c623bdf7
Patrick Wright <patrick@chef.io>
2016-09-19 11:53:27 -0700

    unset vars script

---
+++ acceptance/.ci/unset_vars.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+for ruby_env_var in _ORIGINAL_GEM_PATH \
+                    BUNDLE_BIN_PATH \
+                    BUNDLE_GEMFILE \
+                    GEM_HOME \
+                    GEM_PATH \
+                    GEM_ROOT \
+                    RUBYLIB \
+                    RUBYOPT \
+                    RUBY_ENGINE \
+                    RUBY_ROOT \
+                    RUBY_VERSION
+
+do
+  unset $ruby_env_var
+done
