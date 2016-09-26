77d278bb0309dc74cdbf9a9667b1e1099f5faf6a
Patrick Wright <patrick@chef.io>
2016-09-26 10:45:30 -0700

    skip acceptance in before-script as well in travis

--- ci/before-script.sh
+++ ci/before-script.sh
@@ -19,13 +19,17 @@ do
   unset $ruby_env_var
 done
 
-# download terraform
-wget "https://releases.hashicorp.com/terraform/0.7.4/terraform_0.7.4_linux_amd64.zip"
+# Don't run acceptance tests on forks. The decryption keys are not available.
+if [ "${TRAVIS_REPO_SLUG}" = "chef/mixlib-install" ]; then
 
-# inflate archive
-unzip terraform_0.7.4_linux_amd64.zip -d bin
+  # download terraform
+  wget "https://releases.hashicorp.com/terraform/0.7.4/terraform_0.7.4_linux_amd64.zip"
 
-# decrypt pem
-openssl aes-256-cbc -K $encrypted_e2edbb28e76c_key -iv $encrypted_e2edbb28e76c_iv -in ci/es-infrastructure.pem.enc -out es-infrastructure.pem -d
-mkdir -p ~/.ssh
-mv es-infrastructure.pem ~/.ssh
+   # inflate archive
+   unzip terraform_0.7.4_linux_amd64.zip -d bin
+
+   # decrypt pem
+   openssl aes-256-cbc -K $encrypted_e2edbb28e76c_key -iv $encrypted_e2edbb28e76c_iv -in ci/es-infrastructure.pem.enc -out es-infrastructure.pem -d
+   mkdir -p ~/.ssh
+   mv es-infrastructure.pem ~/.ssh
+fi
