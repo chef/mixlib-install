60536a7714f7fdc86380561c6c2271dd35f9c18e
Patrick Wright <patrick@chef.io>
2017-08-16 13:16:09 -0700

    acceptance provision comments
    Signed-off-by: Patrick Wright <patrick@chef.io>

--- acceptance/ubuntu_install_command_once/.acceptance/acceptance-cookbook/recipes/provision.rb
+++ acceptance/ubuntu_install_command_once/.acceptance/acceptance-cookbook/recipes/provision.rb
@@ -1,3 +1,4 @@
+# Generate install script then write to disk for terraform to copy to the instance for execution
 execute "bundle exec ruby -e \"require 'mixlib/install'; puts Mixlib::Install.new(product_name: 'chef', product_version: :latest, channel: :stable, install_command_options: {install_strategy: 'once'}).install_command\" > ../.acceptance_data/ubuntu_install_command_once.sh" do
   cwd node['chef-acceptance']['suite-dir']
 end
--- acceptance/windows-server-2012r2-once/.acceptance/acceptance-cookbook/recipes/provision.rb
+++ acceptance/windows-server-2012r2-once/.acceptance/acceptance-cookbook/recipes/provision.rb
@@ -1,3 +1,4 @@
+# Generate install script then write to disk for terraform to copy to the instance for execution
 execute "bundle exec ruby -e \"require 'mixlib/install'; puts Mixlib::Install.install_ps1, 'install -install_strategy once'\" > ../.acceptance_data/powershell_install_once.ps1" do
   cwd node['chef-acceptance']['suite-dir']
 end
