15bd68c8ad74e3819c23c0228e6b79c10211c24c
Patrick Wright <patrick@chef.io>
2017-08-24 14:27:44 -0700

    remove duplicate acceptance test
    Signed-off-by: Patrick Wright <patrick@chef.io>

--- acceptance/windows-server-2012r2-url-override/.acceptance/acceptance-cookbook/recipes/destroy.rb
+++
@@ -1,3 +0,0 @@
-execute "terraform destroy -force" do
-  cwd "#{node['chef-acceptance']['suite-dir']}/terraform"
-end
--- acceptance/windows-server-2012r2-url-override/inspec/verify.rb
+++
@@ -1,4 +0,0 @@
-describe package("Chef Client v13*") do
-  it { should be_installed }
-  its('version') { should match /13.2.20/ }
-end
