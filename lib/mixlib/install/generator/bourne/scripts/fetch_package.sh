42c36b2bd0a14444bb1c366f6610ca8170426404
Patrick Wright <patrick@chef.io>
2017-08-31 08:06:56 -0700

    download_url_override and checksum updates for install.sh
    Signed-off-by: Patrick Wright <patrick@chef.io>

--- lib/mixlib/install/generator/bourne/scripts/fetch_package.sh
+++ lib/mixlib/install/generator/bourne/scripts/fetch_package.sh
@@ -31,20 +31,54 @@ download_dir=`dirname $download_filename`
 (umask 077 && mkdir -p $download_dir) || exit 1
 
 # check if we have that file locally available and if so verify the checksum
+# Use cases
+# 1) metadata - new download
+# 2) metadata - cached download when cmdline_dl_dir set
+# 3) url override - no checksum new download
+# 4) url override - with checksum new download
+# 5) url override - with checksum cached download when cmdline_dl_dir set
+
 cached_file_available="false"
+verify_checksum="true"
+
 if test -f $download_filename; then
-  echo "$download_filename already exists, verifiying checksum..."
-  if do_checksum "$download_filename" "$sha256"; then
-    echo "checksum compare succeeded, using existing file!"
-    cached_file_available="true"
+  echo "$download_filename exists"
+  cached_file_available="true"
+fi
+
+if test "x$download_url_override" != "x"; then
+  echo "Download URL override specified"
+  if test "x$cached_file_available" = "xtrue"; then
+    echo "Verifying local file"
+    if test "x$sha256" = "x"; then
+      echo "Checksum not specified, ignoring existing file"
+      cached_file_available="false" # download new file
+      verify_checksum="false" # no checksum to compare after download
+    elif do_checksum "$download_filename" "$sha256"; then
+      echo "Checksum match, using existing file"
+      cached_file_available="true" # don't need to download file
+      verify_checksum="false" # don't need to checksum again
+    else
+      echo "Checksum mismatch, ignoring existing file"
+      cached_file_available="false" # download new file
+      verify_checksum="true" # checksum new downloaded file
+    fi
   else
-    echo "checksum mismatch, downloading latest version of the file"
+    echo "$download_filename not found"
+    cached_file_available="false" # download new file
+    if test "x$sha256" = "x"; then
+      verify_checksum="false" # no checksum to compare after download
+    else
+      verify_checksum="true" # checksum new downloaded file
+    fi
   fi
 fi
 
-# download if no local version of the file available
 if test "x$cached_file_available" != "xtrue"; then
-  do_download "$download_url"  "$download_filename"
+  do_download "$download_url" "$download_filename"
+fi
+
+if test "x$verify_checksum" = "xtrue"; then
   do_checksum "$download_filename" "$sha256" || checksum_mismatch
 fi
 
