f583f4727315851ba0b0c50d1b995ed1c47bfc6c
Tim Smith <tsmith@chef.io>
2018-12-06 10:19:06 -0800

    Update and fix copyrights
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- lib/mixlib/install/backend.rb
+++ lib/mixlib/install/backend.rb
@@ -1,6 +1,6 @@
 #
 # Author:: Patrick Wright (<patrick@chef.io>)
-# Copyright:: Copyright (c) 2015 Chef, Inc.
+# Copyright:: Copyright (c) 2015-2018 Chef Software, Inc.
 # License:: Apache License, Version 2.0
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
--- lib/mixlib/install/script_generator.rb
+++ lib/mixlib/install/script_generator.rb
@@ -1,7 +1,7 @@
 #
 # Author:: Thom May (<thom@chef.io>)
 # Author:: Patrick Wright (<patrick@chef.io>)
-# Copyright:: Copyright (c) 2015 Chef, Inc.
+# Copyright:: Copyright (c) 2015-2018 Chef Software, Inc.
 # License:: Apache License, Version 2.0
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
