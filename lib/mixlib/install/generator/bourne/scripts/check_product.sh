70468a4d72199f26bcc6e2f7ed19d83568ebea03
Stromweld <hemminger@hotmail.com>
2025-12-30 15:04:24 -0600

    add install strategy check
    Signed-off-by: Stromweld <hemminger@hotmail.com>

--- lib/mixlib/install/generator/bourne/scripts/check_product.sh
+++ lib/mixlib/install/generator/bourne/scripts/check_product.sh
@@ -1,6 +1,6 @@
 # Check for chef-client command in various locations
 for path in /usr/bin/$project-client /hab/pkgs/chef/chef-infra-client/*/*/bin/chef-client /opt/$project/bin/$project-client; do
-  if test -x "$path" 2>/dev/null; then
+  if test -x "$path" 2>/dev/null && test "x$install_strategy" = "xonce"; then
     echo "$project installation detected at $path"
     echo "install_strategy set to 'once'"
     echo "Nothing to install"
