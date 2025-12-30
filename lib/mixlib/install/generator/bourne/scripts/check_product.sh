69e67da57b3d198164c8302e566e6249ea9b77e5
Stromweld <hemminger@hotmail.com>
2025-12-30 14:58:39 -0600

    fix cinc support
    Signed-off-by: Stromweld <hemminger@hotmail.com>

--- lib/mixlib/install/generator/bourne/scripts/check_product.sh
+++ lib/mixlib/install/generator/bourne/scripts/check_product.sh
@@ -1,5 +1,5 @@
 # Check for chef-client command in various locations
-for path in /usr/bin/chef-client /hab/pkgs/chef/chef-infra-client/*/*/bin/chef-client /opt/chef/bin/chef-client; do
+for path in /usr/bin/$project-client /hab/pkgs/chef/chef-infra-client/*/*/bin/chef-client /opt/$project/bin/$project-client; do
   if test -x "$path" 2>/dev/null; then
     echo "$project installation detected at $path"
     echo "install_strategy set to 'once'"
