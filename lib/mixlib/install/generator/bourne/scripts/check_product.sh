6debbf488b7822c84b59802fd69acc6fa6cc1e4c
Stromweld <hemminger@hotmail.com>
2025-12-30 13:03:41 -0600

    add hab pkg pathing support to skip installs script
    Signed-off-by: Stromweld <hemminger@hotmail.com>

--- lib/mixlib/install/generator/bourne/scripts/check_product.sh
+++ lib/mixlib/install/generator/bourne/scripts/check_product.sh
@@ -1,6 +1,9 @@
-if test -d "/opt/$project" && test "x$install_strategy" = "xonce"; then
-  echo "$project installation detected"
-  echo "install_strategy set to 'once'"
-  echo "Nothing to install"
-  exit
-fi
+# Check for chef-client command in various locations
+for path in /usr/bin/chef-client /hab/pkgs/chef/chef-infra-client/*/*/bin/chef-client /opt/chef/bin/chef-client; do
+  if test -x "$path" 2>/dev/null; then
+    echo "$project installation detected at $path"
+    echo "install_strategy set to 'once'"
+    echo "Nothing to install"
+    exit
+  fi
+done
