91df75c7aac2e72d232d8a07d025cbc66273ada2
Stromweld <hemminger@hotmail.com>
2025-12-31 08:45:49 -0600

    update bin check order to prefer hab install
    Signed-off-by: Stromweld <hemminger@hotmail.com>

--- lib/mixlib/install/generator/bourne/scripts/check_product.sh
+++ lib/mixlib/install/generator/bourne/scripts/check_product.sh
@@ -1,5 +1,5 @@
 # Check for chef-client command in various locations
-for path in "/usr/bin/${project}-client" /hab/pkgs/chef/chef-infra-client/*/*/bin/chef-client "/opt/$project/bin/${project}-client"; do
+for path in /hab/pkgs/chef/chef-infra-client/*/*/bin/chef-client "/usr/bin/${project}-client" "/opt/$project/bin/${project}-client"; do
   if test -x "$path" 2>/dev/null && test "x$install_strategy" = "xonce"; then
     echo "$project installation detected at $path"
     echo "install_strategy set to 'once'"
