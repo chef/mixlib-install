023fb4a670b768ab5431e30125451476c283d299
Corey Hemminger <hemminger@hotmail.com>
2026-01-06 16:03:50 -0600

    add Hab pkg pathing detection to generated script (#407)
    Adds habitat pkg pathing support to determine if chef-client is already installed while maintaining backward compatibility for omnibus pkg pathing.
    
    Updating powershell script to write-host instead of write-verbose. This offers users more info to what's going on in test-kitchen instead of just seeing a module name and no output of download and install status.
    
    ----------------------------
    
    Signed-off-by: Stromweld <hemminger@hotmail.com>
    Co-authored-by: Phil Dibowitz <phil@ipom.com>

--- lib/mixlib/install/generator/bourne/scripts/check_product.sh
+++ lib/mixlib/install/generator/bourne/scripts/check_product.sh
@@ -1,6 +1,9 @@
-if test -d "/opt/$project" && test "x$install_strategy" = "xonce"; then
-  echo "$project installation detected"
-  echo "install_strategy set to 'once'"
-  echo "Nothing to install"
-  exit
-fi
+# Check for chef-client command in various locations
+for path in /hab/pkgs/chef/chef-infra-client/*/*/bin/chef-client "/usr/bin/${project}-client" "/opt/$project/bin/${project}-client"; do
+  if test -x "$path" 2>/dev/null && test "x$install_strategy" = "xonce"; then
+    echo "$project installation detected at $path"
+    echo "install_strategy set to 'once'"
+    echo "Nothing to install"
+    exit
+  fi
+done
