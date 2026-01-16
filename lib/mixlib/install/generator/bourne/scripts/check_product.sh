4239d7deb0fdc473b5baadff30385f8d1d706238
Corey Hemminger <hemminger@hotmail.com>
2026-01-16 13:32:21 -0600

    optimize shell check code
    Signed-off-by: Corey Hemminger <hemminger@hotmail.com>

--- lib/mixlib/install/generator/bourne/scripts/check_product.sh
+++ lib/mixlib/install/generator/bourne/scripts/check_product.sh
@@ -1,22 +1,17 @@
 # Check for product installation in various locations
 if [ "$project" = "chef" ] || [ "$project" = "chef-ice" ]; then
   # For chef or chef-ice, look for chef-infra-client paths
-  for path in /hab/pkgs/chef/chef-infra-client/*/*/bin /opt/chef/bin; do
-    if test -d "$path" 2>/dev/null && test "x$install_strategy" = "xonce"; then
-      echo "$project installation detected at $path"
-      echo "install_strategy set to 'once'"
-      echo "Nothing to install"
-      exit
-    fi
-  done
+  install_paths="/hab/pkgs/chef/chef-infra-client/*/*/bin /opt/chef/bin"
 else
   # For other products, look for product-specific paths
-  for path in /hab/pkgs/chef/$project/*/*/bin /opt/$project/bin; do
-    if test -d "$path" 2>/dev/null && test "x$install_strategy" = "xonce"; then
-      echo "$project installation detected at $path"
-      echo "install_strategy set to 'once'"
-      echo "Nothing to install"
-      exit
-    fi
-  done
+  install_paths="/hab/pkgs/chef/$project/*/*/bin /opt/$project/bin"
 fi
+
+for path in $install_paths; do
+  if [ -d "$path" ] 2>/dev/null && [ "x$install_strategy" = "xonce" ]; then
+    echo "$project installation detected at $path"
+    echo "install_strategy set to 'once'"
+    echo "Nothing to install"
+    exit
+  fi
+done
